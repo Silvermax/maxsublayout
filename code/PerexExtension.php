ddd3665bc0955e2f813278610c893e2da6ab537a
Pali Ondras <pali.ondras@gmail.com>
2014-02-26 10:47:55 +0100

    tunning up hasReadMoreLink method

--- code/PerexExtension.php
+++ code/PerexExtension.php
@@ -10,12 +10,18 @@ class PerexExtension extends DataExtension {
 		$fields->addFieldToTab("Root.Perex", new HtmlEditorField("Perex","Perex"));
 	}
 	
+	/***
+	 * No perex means, Content is used as Perex. hasReadmoreLink() rerutns false in this screnario!  
+	 */
 	public function Perex() {
 		return ($this->owner->Perex) ? $this->owner->Perex : $this->owner->Content;
 	}
 	
+	/*** 
+	 * This is too generic method. Use custom version on your page classes
+	 */
 	public function hasReadMoreLink() {
-		return ((($this->owner->Content && $this->owner->Perex) || ($this->owner instanceof RedirectorPage)) && Controller::curr()->ID != $this->owner->ID) ? 1 : 0;
+		return ((($this->owner->Content && $this->owner->Perex) || ($this->owner instanceof RedirectorPage) || $this->owner->Children()) && Controller::curr()->ID != $this->owner->ID) ? true : false;
 	}
 					
 }
